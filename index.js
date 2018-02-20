4db497d4d0a22b0995cd93daf975ac79aba249d0
connorjohnlind <connorjohnlind@gmail.com>
2018-02-20 09:13:01 -0800

    Added babelrc file

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/dist'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
